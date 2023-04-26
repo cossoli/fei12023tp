461844cbf8157eea9776de52fe8fc3bb2154bbf3
dario <cochocossolidario@gmail.com>
2023-04-25 22:28:19 -0300

    punto 13  uso de JSON

--- backend/basic/config/web.php
+++ backend/basic/config/web.php
@@ -15,6 +15,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'hu8wm5ChxQr6Zu2u8eQ4bRcZLpkX60xg',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
